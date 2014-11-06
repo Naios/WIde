da0f4b53515a75a7227c35164c11708a2076ffe6
Naios <naios-dev@live.de>
2014-11-06 02:05:37 +0100

    Sync

---
+++ src/main/java/wide/bootstrap/Main.java
@@ -0,0 +1,31 @@
+package wide.bootstrap;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        /*
+     // create Options object
+        Options options = new Options();
+
+        // add t option
+        options.addOption("t", false, "display current time");
+        
+        
+        CommandLineParser parser = new DefaultParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse( options, args );
+        }
+        catch( ParseException exp ) {
+            // oops, something went wrong
+            System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+        }
+        */
+    }
+}
--- src/main/java/wide/core/Main.java
+++
@@ -1,9 +0,0 @@
-package wide.core;
-
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
