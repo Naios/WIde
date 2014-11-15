aeceb7039b0c359f6363ab1c4e68660b1ce90294
Naios <naios-dev@live.de>
2014-11-15 05:52:31 +0100

    Made some internal config strings more package like

--- src/main/java/wide/core/session/config/ConfigEntry.java
+++ src/main/java/wide/core/session/config/ConfigEntry.java
@@ -2,17 +2,17 @@ package wide.core.session.config;
 
 public enum ConfigEntry
 {
-    CONFIG_DATABASE_USER("db.user"),
-    CONFIG_DATABASE_HOST("db.host"),
-    CONFIG_DATABASE_PORT("db.port"),
-    CONFIG_DATABASE_PASSWORD("db.password"),
+    CONFIG_DATABASE_USER("core.session.database.user"),
+    CONFIG_DATABASE_HOST("core.session.database.host"),
+    CONFIG_DATABASE_PORT("core.session.database.port"),
+    CONFIG_DATABASE_PASSWORD("core.session.database.password"),
 
-    CONFIG_DATABASE_AUTH("db.auth"),
-    CONFIG_DATABASE_CHARACTER("db.character"),
-    CONFIG_DATABASE_WORLD("db.world"),
+    CONFIG_DATABASE_AUTH("core.session.database.auth"),
+    CONFIG_DATABASE_CHARACTER("core.session.database.character"),
+    CONFIG_DATABASE_WORLD("core.session.database.world"),
 
-    CONFIG_DATABASE_AUTOLOGIN("db.autologin"),
-    CONFIG_DATABASE_SAVE_PASSWORD("db.savepassword");
+    CONFIG_DATABASE_AUTOLOGIN("core.session.database.autologin"),
+    CONFIG_DATABASE_SAVE_PASSWORD("core.session.database.savepassword");
 
     private final String storageName;
 
