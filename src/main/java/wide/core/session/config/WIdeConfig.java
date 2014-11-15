a3bd8663f360e9c37b56086684c3452573d6ceef
Naios <naios-dev@live.de>
2014-11-15 04:29:54 +0100

    Replaced string constants by config entrys

--- src/main/java/wide/core/session/config/WIdeConfig.java
+++ src/main/java/wide/core/session/config/WIdeConfig.java
@@ -7,14 +7,14 @@ public class WIdeConfig extends Config
     private static final String[][] DEFAULT_PROPERTIES = new String[][]
     {
             // Default properties
-            {"DB:User", "root"},
-            {"DB:Host", "localhost"},
-            {"DB:Port", "3306"},
-            {"DB:Password", ""},
+            {ConfigEntry.CONFIG_DATABASE_USER.getStorageName(), "root"},
+            {ConfigEntry.CONFIG_DATABASE_HOST.getStorageName(), "localhost"},
+            {ConfigEntry.CONFIG_DATABASE_PORT.getStorageName(), "3306"},
+            {ConfigEntry.CONFIG_DATABASE_PASSWORD.getStorageName(), ""},
             {ConfigEntry.CONFIG_DATABASE_AUTH.getStorageName(), "auth"},
             {ConfigEntry.CONFIG_DATABASE_CHARACTER.getStorageName(), "character"},
             {ConfigEntry.CONFIG_DATABASE_WORLD.getStorageName(), "world"},
-            {"DB:Autologin", "false"}
+            {ConfigEntry.CONFIG_DATABASE_AUTOLOGIN.getStorageName(), "false"}
     };
 
     @Override
--- src/main/java/wide/core/session/database/ConfigEntry.java
+++ src/main/java/wide/core/session/database/ConfigEntry.java
@@ -2,9 +2,16 @@ package wide.core.session.database;
 
 public enum ConfigEntry
 {
+    CONFIG_DATABASE_USER("DB:User"),
+    CONFIG_DATABASE_HOST("DB:Host"),
+    CONFIG_DATABASE_PORT("DB:Port"),
+    CONFIG_DATABASE_PASSWORD("DB:Password"),
+
     CONFIG_DATABASE_AUTH("DB:Auth"),
     CONFIG_DATABASE_CHARACTER("DB:Character"),
-    CONFIG_DATABASE_WORLD("DB:World");
+    CONFIG_DATABASE_WORLD("DB:World"),
+
+    CONFIG_DATABASE_AUTOLOGIN("DB:Autologin");
 
     private final String storageName;
 
