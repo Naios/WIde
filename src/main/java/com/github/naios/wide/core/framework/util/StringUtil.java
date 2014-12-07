3a3ef29d309098bdd2ae1126e706229e1b52be2d
Naios <naios-dev@live.de>
2014-12-07 05:31:07 +0100

    Added StringUtil::concat

--- src/main/java/com/github/naios/wide/core/framework/util/StringUtil.java
+++ src/main/java/com/github/naios/wide/core/framework/util/StringUtil.java
@@ -18,4 +18,17 @@ public class StringUtil
                 .replaceAll("_{2}", "_")
                 .replaceAll("[()'@\"]", "");
     }
+
+    public static String concat(final Object[] array, final String delemiter)
+    {
+        final StringBuilder builder = new StringBuilder();
+
+        for (final Object obj : array)
+            builder.append(obj.toString()).append(delemiter);
+
+        if (builder.length() >= delemiter.length())
+            builder.delete(builder.length() - delemiter.length(), builder.length());
+
+        return builder.toString();
+    }
 }
