9d5381d3d3f727667310e9578e8f7b6cf3771045
Naios <naios-dev@live.de>
2014-12-09 05:13:29 +0100

    Licensed the Project under Creative Commons 4.0 (by-nc-sa) * Feel free to contribute * You are not allowed to use it in commercial projects where   you get money through donations or sells, private servers for example. * If you create content with this application/framwork share it with others!

--- src/main/java/com/github/naios/wide/core/framework/game/UnitClass.java
+++ src/main/java/com/github/naios/wide/core/framework/game/UnitClass.java
@@ -1,3 +1,11 @@
+
+/*
+ * Copyright (c) 2013 - 2014 Naios <naios-dev@live.de>
+ *
+ * This file is part of WIde which is released under Creative Commons 4.0 (by-nc-sa)
+ * See file LICENSE for full license details.
+ */
+
 package com.github.naios.wide.core.framework.game;
 
 public enum UnitClass
