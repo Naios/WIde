26d23f964166febc3231be0d36349604c8f3b3f6
Naios <naios-dev@live.de>
2014-12-10 13:03:28 +0100

    Add SQLVariable class

---
+++ src/main/java/com/github/naios/wide/core/framework/storage/server/builder/SQLVariable.java
@@ -0,0 +1,59 @@
+
+/*
+ * Copyright (c) 2013 - 2014 Naios <naios-dev@live.de>
+ *
+ * This file is part of WIde which is released under Creative Commons 4.0 (by-nc-sa)
+ * See file LICENSE for full license details.
+ */
+
+package com.github.naios.wide.core.framework.storage.server.builder;
+
+public class SQLVariable
+{
+    final String name, value;
+
+    public SQLVariable(final String name, final String value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final SQLVariable other = (SQLVariable) obj;
+        if (name == null)
+        {
+            if (other.name != null)
+                return false;
+        }
+        else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+}
