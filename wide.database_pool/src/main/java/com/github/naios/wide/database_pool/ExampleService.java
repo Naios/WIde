ccbb2718255ab00e1a972cadf3f617c9ba7fb7cb
Naios <naios-dev@live.de>
2014-12-24 09:15:43 +0100

    add WIde database-pool

---
+++ wide.database_pool/src/main/java/com/github/naios/wide/database_pool/ExampleService.java
@@ -0,0 +1,12 @@
+package com.github.naios.wide.database_pool;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
