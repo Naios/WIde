c36baf69da917d27da458e2d137617478eaab37f
Naios <naios-dev@live.de>
2015-01-03 18:59:00 +0100

    set aries blueprint startlevel to 2

--- wide.framework/src/main/java/com/github/naios/wide/framework/internal/FrameworkServiceImpl.java
+++ wide.framework/src/main/java/com/github/naios/wide/framework/internal/FrameworkServiceImpl.java
@@ -50,15 +50,77 @@ public final class FrameworkServiceImpl implements FrameworkService
                 final ClientStorage<MapEntry> me = new ClientStorageSelector<MapEntry>("Map.dbc").select();
                 System.out.println(String.format("DEBUG: %s", me));
 
-                final ServerStorage<CreatureTemplate> ct = new ServerStorageImpl<>("world", "creature_template");
+                final ServerStorage<CreatureTemplate> table = new ServerStorageImpl<>("world", "creature_template");
 
-                System.out.println(String.format("DEBUG: %s", ct.get(new ServerStorageKey<>(41378))));
+                System.getProperties().forEach((key, value) -> System.out.println(String.format("%s = %s", key, value)));
 
-                ct.getWhere("entry between 22000 and 30000 limit 20").forEach(template -> System.out.println(template));
-            }
+                System.out.println(String.format("DEBUG: %s", table.get(new ServerStorageKey<>(41378))));
+
+                table.getWhere("entry between 22000 and 30000 limit 20").forEach(template -> System.out.println(template));
+
+                /*
+                final CreatureTemplate ct1 = table.get(CreatureTemplate.createKey(491));
+                final CreatureTemplate ct2 = table.get(CreatureTemplate.createKey(41378));
+                final CreatureTemplate ct3 = table.get(CreatureTemplate.createKey(151));
+                final CreatureTemplate ct4 = table.get(CreatureTemplate.createKey(69));
+
+                table.getChangeHolder().setScope("test scope", "simple modify test");
+
+                ct1.name().set("blub");
+
+                ct2.name().set("blub");
+
+                ct3.unit_class().set(UnitClass.CLASS_ROGUE);
+
+                ct3.kill_credit1().set(123456);
+                table.getChangeHolder().setCustomVariable(ct3.kill_credit1(), "credit custom variable");
+
+                table.getChangeHolder().setScope(
+                        "test flag scope",
+                        "some flag tests\nadds some strange flags to maloriak\n"
+                                + "it only updates flags that have changed");
+
+                ct2.unit_flags().removeFlag(UnitFlags.UNIT_FLAG_UNK_6);
+
+                // This flag is not present in the database and won't lead to changes
+                ct2.unit_flags().removeFlag(UnitFlags.UNIT_FLAG_DISARMED);
+
+                ct2.unit_flags().addFlag(UnitFlags.UNIT_FLAG_IMMUNE_TO_NPC);
+                ct2.unit_flags().addFlag(UnitFlags.UNIT_FLAG_NOT_SELECTABLE);
+
+                table.getChangeHolder().setScope("delete scope", "deletes a creature template");
+                ct4.delete();
+
+                table.getChangeHolder().setScope("delete scope 2", "now we wanna delete multiple entrys, yay!");
+                for (int i = 115; i < 120; ++i)
+                {
+                    final CreatureTemplate deleteMe = table.get(CreatureTemplate.createKey(i));
+                    if (deleteMe != null)
+                        deleteMe.delete();
+                }
+
+                table.getChangeHolder().setScope("create scope 1", "creates one new creature template...");
+                final CreatureTemplate myqueryentry = table.create(CreatureTemplate.createKey(1000000));
+                myqueryentry.name().set("my test name");
+
+                table.getChangeHolder().setScope("create scope 2", "creates 5 templates with random values");
+                for (int i = 2000000; i < 2000005; ++i)
+                {
+                    final CreatureTemplate template = table.create(CreatureTemplate.createKey(i));
+
+                    template.unit_class().set(RandomUtil.getInt(0, 3));
+                    template.unit_flags().set(RandomUtil.getInt(0, 30));
+                    template.kill_credit1().set(RandomUtil.getInt(0, 10000));
+                    template.name().set(RandomUtil.getString(RandomUtil.getInt(3, 15)));
+                }
+
+                System.out.println(table.getChangeHolder());
+                System.out.println(table.getChangeHolder().getQuery());
+                */
+             }
 
 
-        }).start();
+        })/*.start()*/;
     }
 
     public void stop()
