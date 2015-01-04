8bf4803421aa461169840b9b5d653b2726f0b129
Naios <naios-dev@live.de>
2015-01-04 13:45:49 +0100

    readd flagspitter ("flags") and show enum ("enums") command

--- wide.entities/src/main/java/com/github/naios/wide/entities/internal/EntityServiceImpl.java
+++ wide.entities/src/main/java/com/github/naios/wide/entities/internal/EntityServiceImpl.java
@@ -9,14 +9,18 @@
 package com.github.naios.wide.entities.internal;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.felix.service.command.Descriptor;
 import org.osgi.framework.Bundle;
 
 import com.github.naios.wide.api.entities.EntityService;
 import com.github.naios.wide.api.entities.NoSucheEntityException;
 import com.github.naios.wide.api.framework.storage.client.ClientStorageStructure;
 import com.github.naios.wide.api.framework.storage.server.ServerStorageStructure;
+import com.github.naios.wide.api.util.FlagUtil;
+import com.github.naios.wide.api.util.StringUtil;
 import com.github.naios.wide.entities.enums.Classes;
 
 public class EntityServiceImpl implements EntityService
@@ -108,5 +112,54 @@ public class EntityServiceImpl implements EntityService
             throw new NoSucheEntityException(fullName, ClientStorageStructure.class);
 
         return (Class)type;
+    }
+
+    /**
+     * Converts hex into int if necessary
+     */
+    private int valueConverter(final String value)
+    {
+        if (value.startsWith("0x"))
+            return Integer.parseInt(value.substring(2), 16);
+        else
+            return Integer.valueOf(value);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Descriptor("Shows an enum constant of the given enum")
+    public void enums(@Descriptor("The enum name (UnitFlags, UnitClass for example)") final String name,
+            @Descriptor("The value you want to translate (in decimal or hex)") final String value)
+    {
+        final int val = valueConverter(value);
+        final Class<? extends Enum> enumeration = requestEnum(name);
+
+        final Enum enumValue;
+        try
+        {
+            enumValue = enumeration.getEnumConstants()[val];
+        }
+        catch (final Exception e)
+        {
+            System.out.println(String.format("Value %s is not a part of Enum %s.", val, name));
+            return;
+        }
+
+        System.out.println(String.format("Value: %s = %s = %s", value, StringUtil.asHex(val), enumValue.name()));
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Descriptor("Shows all enum flags of the value and the given enum.")
+    public void flags(@Descriptor("The enum name (UnitFlags, UnitClass for example)") final String name,
+            @Descriptor("The flags you want to show (in decimal or hex).") final String value)
+    {
+        final int val = valueConverter(value);
+        final Class<? extends Enum> enumeration = requestEnum(name);
+        final List<? extends Enum> flags = FlagUtil.getFlagList(enumeration, val);
+
+
+        System.out.println(String.format("Value: %s = %s", value, StringUtil.asHex(val)));
+
+        for (final Enum flag : flags)
+            System.out.println(String.format("\n%-10s = %s", StringUtil.asHex(FlagUtil.createFlag(flag)), flag.name()));
     }
 }
