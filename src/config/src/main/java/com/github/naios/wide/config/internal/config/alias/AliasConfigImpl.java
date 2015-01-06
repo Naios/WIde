d94bb766dba0e7902f0061db8f6c61778dd7a5ed
Naios <naios-dev@live.de>
2015-01-06 15:31:23 +0100

    Add implementation of AliasConfig

--- src/config/src/main/java/com/github/naios/wide/config/internal/config/alias/AliasConfigImpl.java
+++ src/config/src/main/java/com/github/naios/wide/config/internal/config/alias/AliasConfigImpl.java
@@ -8,49 +8,68 @@
 
 package com.github.naios.wide.config.internal.config.alias;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+import com.github.naios.wide.api.WIdeConstants;
 import com.github.naios.wide.api.config.alias.Alias;
 import com.github.naios.wide.api.config.alias.AliasConfig;
 
+@SuppressWarnings("serial")
+class MissingAliasException extends RuntimeException
+{
+    public MissingAliasException(final String name)
+    {
+        super(String.format("Alias %s is missing!!", name));
+    }
+}
+
 public class AliasConfigImpl implements AliasConfig
 {
+    private StringProperty name = new SimpleStringProperty(""),
+            desciption = new SimpleStringProperty(""),
+                    version = new SimpleStringProperty(WIdeConstants.VERSION_WIDE_ALIAS_CONFIG.toString());
+
+    private Map<String, AliasImpl> aliases = new HashMap<>();
+
     @Override
     public StringProperty name()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return name;
     }
 
     @Override
     public StringProperty description()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return desciption;
     }
 
     @Override
     public StringProperty version()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return version;
     }
 
     @Override
     public Alias getAliasForName(final String name)
     {
-        // TODO Auto-generated method stub
-        return null;
+        final Alias alias = aliases.get(name);
+        if (Objects.nonNull(alias))
+            return alias;
+        else
+            throw new MissingAliasException(name);
     }
 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     public Set<Entry<String, Alias>> getAliases()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return (Set)aliases.entrySet();
     }
-
 }
